6387ec6a8993f1f376a4823fa7face566583842d
DGriffin91 <33357138+DGriffin91@users.noreply.github.com>
2018-08-20 00:03:32 -0700

    Add files via upload

---
+++ sgui/Examples/midiTest.py
@@ -0,0 +1,28 @@
+import sgui
+import mido
+
+port = mido.open_output()
+
+#Start TK window
+window = sgui.Tk() 
+
+#Layout frame
+mainBox = sgui.HBox(window)
+
+
+def playMidiNotes(btn):
+    notes = noteSequenceEntry.string.split()
+    for note in notes:
+        print(int(note))
+        msg = mido.Message('note_on', note=int(note))
+        port.send(msg)
+
+buttonBox = sgui.HBox(mainBox)
+
+noteSequenceEntry = sgui.TextEntry(buttonBox, string = "60 64") 
+
+playButton = sgui.Button(buttonBox, string = "Play", command = playMidiNotes) 
+
+
+#Start GUI
+window.startGUI()
